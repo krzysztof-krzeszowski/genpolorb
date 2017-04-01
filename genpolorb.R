4715a28786c05d7a0616c63782c7c569d8ae5d58
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-04-01 12:57:38 +0200

    small changes

--- genpolorb.R
+++ genpolorb.R
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
 
+# close all devices
+graphics.off()
+
 suppressMessages(library(GA))
 source("fit_functions.R", local=T)
 
@@ -10,15 +13,12 @@ popSize <- 10
 ### FUNCTIONS #########################################
 
 plot.initial <- function() {
-  
+  par(mar=c(6, 15, 2, 2))
   d$phase <- (d$mjd - init.t0) %% init.p / init.p
   
-  ylim <- c(min(d$pd - d$dpd), max(d$pd + d$dpd))
   xlab <- "Orbital phase"
   ylab <- "PD [%]"
   
-  with(d, plot(phase, pd, ylim=ylim, xlab=xlab, ylab=ylab, pch=19))
-  with(d, segments(x0=phase, y0=pd - dpd, y1=pd + dpd))
   
   plot.t <- seq(min(d$mjd), min(d$mjd) + init.p, 0.1)
   plot.pd <- fit.pd.e(plot.t, init.t0, init.p, init.i, init.e, init.o, init.p.max)
@@ -26,8 +26,16 @@ plot.initial <- function() {
   plot.df$phase <- (plot.df$t - init.t0) %% init.p / init.p
   
   plot.df <- plot.df[order(plot.df$phase), ]
+  
+  ylim <- c(min(d$pd - d$dpd, plot.df$pd), max(d$pd + d$dpd, plot.df$pd))
+  
+  with(d, plot(phase, pd, xlim=c(0, 1), ylim=ylim, xlab=xlab, ylab=ylab, pch=19, main="Initial parameters plot"))
+  with(d, segments(x0=phase, y0=pd - dpd, y1=pd + dpd))
   with(plot.df, lines(phase, pd))
+  mtext(text=paste("t0 =", t0), side=2, las=1, padj=0, adj=0)
 }
+
+pdf(file="orb_fit.pdf", title="orb fit", width=9, height=5)
 
 ### READ DATA #########################################
 
@@ -38,21 +46,20 @@ d <- d[, c("mjd", "pd", "dpd")]
 
 ### SET INITIAL PARAMETERS ############################
 
-init.t0 <- 57225.068
+init.t0 <- 57150.068
 init.p <- 33.83
 init.i <- 10.3
 init.e <- 0.37
-init.o <- 7.8
+init.o <- 280
 init.p.max <- 3.84
 
-lower <- list(t0=init.t0 - 101.16, p=init.p - 3, i=0,  e=0.365, o=0, p.max=3.5)
-upper <- list(t0=init.t0 + 101.16, p=init.p + 3, i=90, e=0.375, o=180, p.max=4.0)
+lower <- list(t0=init.t0 - 200, p=init.p - 10, i=0,  e=0.0, o=0, p.max=3.5)
+upper <- list(t0=init.t0 + 200, p=init.p + 10, i=90, e=0.95, o=360, p.max=4.0)
 
 
 ### PLOT INITIAL CURVE ################################
 
 plot.initial()
-
 
 ### FIT TEST DATA #####################################
 test = F
@@ -105,7 +112,7 @@ fit <- nls(y ~ fit.pd.e(x, t0, p, i, e, o, p.max),
            algorithm="port")
 
 
-
+dev.off()
 
 
 
